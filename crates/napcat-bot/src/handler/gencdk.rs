f25d51dc54ee88ba57dcab97c0fe34a65927cd42
leibuyun <leibuyun@wps.cn>
2026-05-29 09:45:27 +0800

    feat: update cdk service

--- crates/napcat-bot/src/handler/gencdk.rs
+++ crates/napcat-bot/src/handler/gencdk.rs
@@ -4,7 +4,7 @@ use onebot::api::payload::SendGroupMsg;
 use onebot::event::message::GroupMessageEvent;
 use onebot::message::MessageSegment;
 use onebot::Message;
-use rsa::pkcs1::DecodeRsaPrivateKey;
+use rsa::pkcs8::DecodePrivateKey;
 use rsa::pkcs1v15::SigningKey;
 use rsa::signature::SignatureEncoding;
 use rsa::signature::Signer;
@@ -12,33 +12,34 @@ use sha2::Sha256;
 
 use super::{extract_plain_text, HandlerContext};
 
-const PRIVATE_KEY_PEM: &str = "-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAvgQ0tway0AJLCQiHcJRf9n325WNS1o1o62yBz8gRprB1LBFd
-L84hhlmzIipc3abLsQra9BObFLGeIsiz3nwweCmgi1sNErPKfBq9+YsUci4SbIsM
-Iu8l01RAaOqTFVPLUGEYE6fXtngNMIXphICjlSW/2mMp/TVl0qqWnQrIldysyxgt
-WKvbAv0k3naLxkKDyH6PbXZ/yYWaxePKts2xtfAolAJaimOIQkK9TKVkhNIxuPdx
-OP65nV2vC3CKg3xP5wxWurTTNkL4rWK80LUP01wKrfzdp1GztYwCMLONpbnMR36m
-vuTYA717trR9gyDZPWGGXHSxmedLXLtgkfZTqwIDAQABAoIBAQCYqexoeFtFv/Hl
-ShL3Il6PPdkVp10wv+Bh9YW+GLIFyJP7WeASvnw04vCHLJ37/zx7+4q6ut3IHIQ+
-0h2hTQnsIRW5oOe59PVkDGBBk7pTmix3RKf1kUpEpdYx9PVDF1WsOLYNZLZtBbsj
-FxPsvyWueOvRXAaqRzKNtTzY44cQzx7fZx8AmHcipcFyaym0uIcpIdZwBC4o85Js
-KMM4Ez8GfaS404MKgxBgqXVeAtqSYD7lQbrUzAx2qosmP1Z/bYYB9CBx2OB2joie
-x0MozRqboBOoc5QGC7XSYgX8KwfFk8i9K5eL/kfXD7aXzdqd5Xrs5PTm2koUoHBb
-ef7XCSIxAoGBAOVrlunEuCNpQ0+rmFb3ITaF9JVkBsmNLuvasKe0CRGH3luBjEUn
-XvN/f3kxTR31Asr4cMKS8ln58D6yNsLC1Oc+9kwLj1DAkyKjwT53cHyAzpXfvkTE
-GQBImszU/KZrOcFiYKOkk+Ua6VMHa+XBW1Un7RbfXVqzxLVQ6jRCTPrdAoGBANQH
-7pFhQSFQF6/gsE8fV08FhtGMJDcITD4ugKYOH47QflyudcDqX5yNCgu9fTQ5708T
-rnZL2vNoi22aQamnS0SuTIrB2UrhgobjhgscZ6v4CiEcHew5DiZha5JYuWwItNnu
-RAnkXnw7piW2J/oYcxUzaiOQ0KlzN02JJ+iKZMwnAoGBAL7O8+4rkebJxpT9p680
-zSfW06xoTAjX5p18/o4Me7pb4YEDxxFBBITKls/KRFRVEeSUKtx5cR2Kddj/SfJE
-LuTBhgGLX8AO2pDl13RHzIOQccFPHKV+3zhQKoeP4S3cYmXHl46i8+qJrmNC+edW
-IMs7cMIkNjWY7FLNIG0kc3f1AoGAYjUGT/n+48IoJoNoxk0a8HP73QUPJRpHzilV
-1xQFk+2ICb+YtPEZtfYxp/xtiIopCLRyA0LhOAq9QdfIAB+HolklBMQCtEc9YOLz
-jCPs9N8fOfS++1H19tr6qz9DKwHhWmucwgQvq1UpgKAdZh5691/oEm8Z5tKB0/zq
-KjAnvdMCgYBchwzW2bs0P0hcX8hUX1pwZn0DYNdvXR20vJYhSV+0oE2EMFgH4gMX
-553QG885HVx+4sIoPt6XgDLGSLInNJmxm8pUYx7Yb5L85fYJfdQESGYqvBbBPcgN
-gHHJ30TRnGGCBZbkRAuEc2mrzKQgHsCLnf66Nyheehjg3SV2s1hTqg==
------END RSA PRIVATE KEY-----";
+const PRIVATE_KEY_PEM: &str = "-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDBCibJbFsYsO5y
+2P0I2eoFy3TsFg0KrqERhEYPP66dERVa4AitJBZWrGggTtGLIlVHkW9m2dqxSW/W
+Ly9ud9CkZozdnwgDQbe+v4N5kewBR0UQQQatsGo2geb7K0Dr3+PM6ScdobC7LZA5
+YefnNX5cp6CmiPSEfnnj21HF4jRwlPqG+/9sPtfDeRd2YBU6vFBe25NevcUDvtjH
+CYX8+ylvXWDxtkBlB11m+cQKNUt9knNo2GUZ0rfbgv2EI1ff6oT5tZQfqwsbs8Qy
+i67i/hQSeQG+omGEVkz3kKQslMjQZ8PKE4ZpDaRTybo448c85uCsraTSXbx/yoqD
+CPC5zclJAgMBAAECggEAEosGeIsKcaPeVeUq4kingqS9f6erG0t00K1RewnBnNgd
+Imh6oOIibLM9Qdw8Y/Z5dWlep4U2DRlqLyuDbYPAJKiur6PFBeYQT9gO+bS3FFTb
+qSDr8q+Ldl6yWM55+yZ7uA1t+tpTMxnAfMifOY4hjCY0iAbIITmSfR0uXNk6sxUr
+so8pnctrJmZTsNoIzr3lAdlJRv/4sSnEkcjZYRPx1y5fdI93qRlQRf8b51352EKy
+f0N6spnEZr7nhIhU/4v8c6U8md0g2l+4QGMyGygngF9rHBRzqIRGsp07I7Nddjc5
+vQpM2MaK98iSsZtqjiGEGXDTNVShZfJXN86X7YcpIQKBgQDtQQ1s9ZYhsEvEkPaf
+Ip3x+wF8YJy8crh6IYxQKkI+WQlEsjJ3T6AyCXrLCOmOGF8Nt9u8r8sfOXu8mY01
+ks4mJhi4V4VS3FIDDSdhfYGfGqY7yRdmNDkQYld0UhZIM18IdCBOVEF4zLeOasRZ
+IEC63J4HmaCB8APVhZWrzivbaQKBgQDQSsWKEBneSNXMo6M/6EdJpW1bdJiCSTPN
+mm0vzsZcgFFUVSbOgSu5AiMevjGWvnacV2DEFVONh+oJD46NyHu5atPC3ZAyWohl
+as8ISZ14Y0xq5o99qkflO/4V6s+KalkXokvvEdQTEyeFYy4bzKX5xkjAlBLf7Ue8
+Fcpss24i4QKBgQDBtfyFOxsiVHP4gTerhLMa8IstByDRyIUQyrVqeqZti3rCyQ/l
+VHECibTlc1hmOUXayIQz0gBxdRivS1v9IukIQtCqKmNj3Rlk/mdp9PRReIvDgpOF
+UhxJYVHwWllxB+iO2WnLKoXuYI96S3gXIPtY1mp84BUqIlKvEou6o/IxCQKBgCRy
+sRZzstMe06q3h72LG85bUEOMp5NE0/fKagjPmg5dtd2X+O5x1ADPyu16QpsqQP8i
+myA0yyYc/msPedZ9mojblKqosq7dALkec5PzrcZ/OcQLDFjlDyeh09hp+l8yNNzZ
+3Ye8Cuw7kdLZhBwBN5n5hImOX68nikHzXjSfQUqBAoGBAMgkEbNl3lRSNWdCpLQS
+jB5Aj8IPJ/ehpOEh1j0FHuRi44ZhB8VuJxxwCMyCqlmfTaHRWSYRc5CE2pT6Qvei
+7iYHYWcP+iCAV1OpveGl9pBLVHn6mqAoWymZch2r9K+IqxKFiBgoaOtc53sWZAlh
+HjVyXb4f/gj01+geWs5fEm62
+-----END PRIVATE KEY-----";
 
 const DEFAULT_DURATION_HOURS: u64 = 2;
 const DEFAULT_SUB: &str = "bot";
@@ -108,7 +109,7 @@ fn generate_cdk(hwid: &str) -> Result<String, Box<dyn std::error::Error>> {
 
     let payload_b64 = B64.encode(payload_json.as_bytes());
 
-    let private_key = rsa::RsaPrivateKey::from_pkcs1_pem(PRIVATE_KEY_PEM)?;
+    let private_key = rsa::RsaPrivateKey::from_pkcs8_pem(PRIVATE_KEY_PEM)?;
     let signing_key = SigningKey::<Sha256>::new(private_key);
     let signature = signing_key.sign(payload_b64.as_bytes());
     let signature_b64 = B64.encode(signature.to_bytes());
